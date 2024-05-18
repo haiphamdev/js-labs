f7cabb982c3c4b6b28006f8a538eaf0c2a80627b
Hai Pham <haipham.ndev@gmail.com>
2024-05-18 18:39:05 +0700

    update docs

--- src/exercise13.js
+++ src/exercise13.js
@@ -1,3 +1,9 @@
+// Viết hàm getFullName(firstName, lastName) nhận vào firstName và lastName và trả về chuỗi fullName.
+// Quy tắc để tạo chuỗi fullName như sau:
+// firstName và lastName là optional (có thể không có)
+// fullName không có khoảng trắng thừa ở đầu và cuối chuỗi
+// firstName và lastName cần phải viết hoa chữ cái đầu tiên, chữ cái còn lại là viết thường
+
 function getFullName(firstName, lastName) {
   // your code here
   const fn = capitalize(firstName || '');
